b9bc8a00ca393be14cb5d4a176f7b461d6be5b1d
S1ROZHA <funlabgames@gmail.com>
2022-12-15 18:57:09 +0200

    test

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('DEPLOY');
+var ping = require('ping');
+
+ping.sys.probe('1.2.3.4', function(isAlive){
+    var msg = isAlive ? 'host ' + ' is alive' : 'host ' + ' is dead';
+    console.log(msg);
+});
